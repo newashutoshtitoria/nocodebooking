693977b8e3815b8567b097eaa09267209b4e4d91
Ashutosh <ashutosh.dev@dreambox.cloud>
2023-01-01 19:36:05 +0530

    token py file change

--- users/token.py
+++ users/token.py
@@ -4,9 +4,9 @@ from datetime import datetime
 def get_tokens_for_user(user, schema_name):
     refresh = RefreshToken.for_user(user)
     refresh.payload['sub']=user.id
-    refresh.payload['iat']=datetime.now()
+    # refresh.payload['iat']=datetime.now()
     refresh.payload['schema_name'] = schema_name
     refresh.access_token.payload['sub']=user.id
-    refresh.access_token.payload['iat']=datetime.now()
+    # refresh.access_token.payload['iat']=datetime.now()
     refresh.access_token.payload['schema_name'] = schema_name
     return str(refresh), str(refresh.access_token)
