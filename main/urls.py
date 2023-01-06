1562ffc47bc2a949aee7f2962ed93c278e5535c9
Ashutosh <ashutosh.dev@dreambox.cloud>
2023-01-06 06:31:43 +0530

    activate user in first tenant view added

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     re_path(r'^user/$', views.Signup.as_view(), name='Signup'),
+    re_path(r'^activate/(?P<user_id>[0-9]+)/$', views.Activate.as_view(), name='activate'),
 
 ]
